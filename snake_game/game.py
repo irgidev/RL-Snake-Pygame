cc198c35e577442aa41ea6f28f7bb9c783e211db
irgidev <irgisetiawan3008@gmail.com>
2025-07-26 14:47:29 +0700

    feat: implement full gameplay loop with growth and collision

--- snake_game/game.py
+++ snake_game/game.py
@@ -1,9 +1,11 @@
 import pygame
+import random
 
 WIDTH = 640
 HEIGHT = 480
 BLACK = (0,0,0)
 GREEN = (0,255,0)
+RED = (255,0,0)
 FPS = 60
 BLOCK_SIZE = 20
 X_CENTER = WIDTH / 2
@@ -15,8 +17,19 @@ screen = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("RL Snake Game")
 clock = pygame.time.Clock()
 
-snake_head = pygame.Rect(X_CENTER,Y_CENTER, BLOCK_SIZE, BLOCK_SIZE)
+snake_body = []
 direction = 'RIGHT'
+
+def create_food():
+    while True:
+        random_food = pygame.Rect(random.randrange(0,WIDTH, BLOCK_SIZE),random.randrange(0,HEIGHT, BLOCK_SIZE), BLOCK_SIZE, BLOCK_SIZE)
+        if random_food not in snake_body:
+            return random_food
+
+food = create_food()
+
+for i in range(3):
+    snake_body.append(pygame.Rect(X_CENTER - (BLOCK_SIZE * i),Y_CENTER, BLOCK_SIZE, BLOCK_SIZE))
 
 running = True
 
@@ -41,19 +54,36 @@ while running:
         last_move_time = now
 
         if direction == 'RIGHT':
-            snake_head.x += BLOCK_SIZE
+            x_new = snake_body[0].x + BLOCK_SIZE
+            y_new = snake_body[0].y
         elif direction == 'LEFT':
-            snake_head.x -= BLOCK_SIZE
+            x_new = snake_body[0].x - BLOCK_SIZE
+            y_new = snake_body[0].y
         elif direction == 'UP':
-            snake_head.y -= BLOCK_SIZE
+            x_new = snake_body[0].x 
+            y_new = snake_body[0].y - BLOCK_SIZE
         elif direction == 'DOWN':
-            snake_head.y += BLOCK_SIZE
+            x_new = snake_body[0].x 
+            y_new = snake_body[0].y + BLOCK_SIZE
+        
+        new_head = pygame.Rect(x_new, y_new, BLOCK_SIZE, BLOCK_SIZE)
+        snake_body.insert(0, new_head)
+        if snake_body[0].colliderect(food):
+            food = create_food()
+        else:
+            snake_body.pop()
 
-    if snake_head.left < 0 or snake_head.right > WIDTH or snake_head.top < 0 or snake_head.bottom > HEIGHT:
+        for i in range(1, len(snake_body)):
+            if snake_body[0].colliderect(snake_body[i]):
+                running = False
+
+        if snake_body[0].left < 0 or snake_body[0].right > WIDTH or snake_body[0].top < 0 or snake_body[0].bottom > HEIGHT:
             running = False
 
     screen.fill(BLACK)
-    pygame.draw.rect(screen,GREEN,snake_head)
+    for i in snake_body:
+        pygame.draw.rect(screen,GREEN,i)
+    pygame.draw.rect(screen,RED,food)
     pygame.display.flip()
     clock.tick(FPS)
 
